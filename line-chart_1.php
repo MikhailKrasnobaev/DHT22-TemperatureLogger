6934bd790a1184564ab00ea8454d1db0e9888288
Михаил Краснобаев <milo1@yandex.ru>
2017-03-13 12:21:57 +0200

    line chart

--- line-chart_1.php
+++ line-chart_1.php
@@ -1,1 +1,54 @@
-
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title><?php echo $cakeDescription ?></title>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                var options = {
+                    chart: {
+                        renderTo: 'container',
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false
+                    },
+                    title: {
+                        text: 'Temp and Humidity Logger'
+                    },
+                    tooltip: {
+                        formatter: function() {
+                            return '<b>' + this.point.name + '</b>: ' + this.y;
+                        }
+                    },
+                    plotOptions: {
+                        line: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: true,
+                                color: '#000000',
+                                connectorColor: '#000000',
+                                formatter: function() {
+                                    return '<b>' + this.point.name + '</b>: ' + this.y;
+                                }
+                            },
+                            showInLegend: true
+                        }
+                    },
+                    series: []
+                };
+                $.getJSON("data/data-line-chart.php", function(json) {
+                    options.series = json;
+                    chart = new Highcharts.Chart(options);
+                });
+            });
+        </script>
+        <script src="http://code.highcharts.com/highcharts.js"></script>
+        <script src="http://code.highcharts.com/modules/exporting.js"></script>
+    </head>
+    <body>
+        <a class="link_header" href="/highcharts/">&lt;&lt; Back to index</a>
+        <div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>
+    </body>
+</html>
